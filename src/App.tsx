02be8b25afdff9d61a803d7039d2bc25b8b56fab
tansionline <tatarselcuk27@gmail.com>
2021-05-28 00:17:01 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <div>
+          <h1>Hello</h1>
+          <p>asd</p>
+        </div>
+    </div>
+  )
+}
+
+export default App
