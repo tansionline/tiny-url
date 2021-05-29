d5d68d11b45706bb03ce5fcb9228f860e4ab1a85
tansionline <tatarselcuk27@gmail.com>
2021-05-29 16:49:34 +0300

    Files structure changed :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import './style/App.css'
 import data from './data/page.json'
-import Input from './components/input'
 
-function App() {
+class App extends React.Component<{}, { value: string }>  {
+  constructor(props: any) {
+    super(props);
+    this.state = { value: '' };
 
-  return (
-    <div>
-      <div className="header">
-        <h1>{data.header}</h1>
-      </div>
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: any) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event: any) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
       <div>
-        <Input />
+        <div className="header">
+          <h1>{data.header}</h1>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="input">
+            <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={data.placeholder} />
+          </div>
+          <div className="button">
+            <button type="submit">{data.button}</button>
+          </div>
+        </form>
       </div>
-    </div>
-  )
+    );
+  }
 }
-
 export default App
