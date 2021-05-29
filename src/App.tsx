f033d130fa6dab9f821c796d5fdff5d6f0fab1b6
tansionline <tatarselcuk27@gmail.com>
2021-05-30 02:10:00 +0300

    Copy input and logic added :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 import './style/App.css'
 import data from './data/page.json'
-import axios from 'axios'
-import 'cors'
 
-class App extends React.Component<{}, { value: string }>  {
+class App extends React.Component<{}, { value: string, url: string }>  {
   constructor(props: any) {
-    super(props);
-    this.state = { value: '' };
+    super(props)
+    this.state = { value: '', url: '' }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event: any) {
-    this.setState({ value: event.target.value });
+    this.setState({ value: event.target.value })
   }
 
   handleSubmit(event: any) {
-
-    console.log(this.state.value)
-
     axios.get(`https://api.1pt.co/addURL?long=${this.state.value}`,)
       .then((response: any) => {
-        console.log(`https://1pt.co/${response.data.short}`);
+        this.setState({ url: `https://1pt.co/${response.data.short}` })
+        console.log(this.state.url)
       })
       .catch(function (error: any) {
-        console.log(error);
+        console.log(error)
       });
-    event.preventDefault();
+    event.preventDefault()
   }
 
   render() {
@@ -40,6 +37,9 @@ class App extends React.Component<{}, { value: string }>  {
         <form onSubmit={this.handleSubmit}>
           <div className="input">
             <input type="text" value={this.state.value} onChange={this.handleChange} placeholder={data.placeholder} />
+          </div>
+          <div className="input">
+            <input type="text" value={this.state.url} onClick={() => { navigator.clipboard.writeText(this.state.url); alert('Copied') }} />
           </div>
           <div className="button">
             <button type="submit">{data.button}</button>
